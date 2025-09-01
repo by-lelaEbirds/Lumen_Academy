79252a771888ac51135623083672b5b864d6f3ad
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-09-01 09:10:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// Funções do site
+function handleSubmit(event) {
+    event.preventDefault();
+    // código de envio do formulário
+}
+
+function handleLead(event) {
+    event.preventDefault();
+    // código de envio do lead
+}
+
+function openModal() {
+    document.getElementById('modal').style.display = 'grid';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+function joinGroup() {
+    // código para redirecionar ao grupo do WhatsApp/Telegram
+}
 
